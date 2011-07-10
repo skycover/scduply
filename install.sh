c93367f88a0051edfda67b3589f317ce61915375
Dmitry Chernyak <dmi.chernyak@gmail.com>
2011-07-10 14:10:17 +0400

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+if [ -z "$1" ]; then
+  target=/usr/local/bin
+  mtarget=/usr/local/share/man/man1
+elif [ -d "$1" ]; then
+  target="$1"
+else
+  echo "Usage $0 'install-path' [man-path]"
+  exit 1
+fi
+echo install to $target
+mkdir -p $target
+if [ -n "$2" ]; then
+  mtarget="$2"
+fi
+echo install man to $mtarget
+mkdir -p $mtarget
+cp scduply "$target"
+cp scduply.1 "$mtarget"
