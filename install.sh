d924842cc3ffef7477496f8be005f3f6067cc104
Dmitry Chernyak <dmi.chernyak@gmail.com>
2011-11-27 14:42:05 +0400

    Improved installation procedure
    The installator is now copies also some files to */share/doc
    
    The new syntax for install.sh:
    ./install.sh [prefix]
    
    The default prefix is /usr/local
    
    The install paths are:
    [prefi]x/bin
    [prefi]x/man/man1
    [prefi]x/share/doc/scduply

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 set -e
 if [ -z "$1" ]; then
-  target=/usr/local/bin
-  mtarget=/usr/local/share/man/man1
+  prefix=/usr/local
 elif [ -d "$1" ]; then
-  target="$1"
+  prefix="$1"
 else
-  echo "Usage $0 ['install-path' [man-path]]"
+  echo "Usage $0 [install-prefix]"
   exit 1
 fi
+target=$prefix/bin
+mtarget=$prefix/share/man/man1
+dtarget=$prefix/share/doc/scduply
 echo install to $target
 mkdir -p $target
 cp scduply "$target"
-if [ -n "$2" ]; then
-  mtarget="$2"
-fi
-if [ -n "$mtarget" ]; then
-  echo install man to $mtarget
-  mkdir -p $mtarget
-  cp scduply.1 "$mtarget"
-fi
+echo install man to $mtarget
+mkdir -p $mtarget
+cp scduply.1 "$mtarget"
+echo install docs to $dtarget
+mkdir -p $dtarget
+for i in CHANGES README; do cp $i "$dtarget"; done
